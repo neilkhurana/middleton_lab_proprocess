dbc4ca72c09d99ec755c3b033bff47bf4194ed21
Middletonlab <Middletonlab@B1F4FX2.ad.upstate.edu>
2019-07-15 14:53:50 -0400

    Initial tabs created for filtering

--- ProProcess/app.R
+++ ProProcess/app.R
@@ -1,49 +1,48 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+## app.R ##
+library(shinydashboard)
 library(shiny)
+library(ggplot2)
+library()
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+ui <- dashboardPage(
+    skin = "purple",
+    dashboardHeader(title = "ProProcess"),
+    dashboardSidebar(sidebarMenu
+                     (
+        menuItem("Home", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("Filtering", tabName = "widgets", icon = icon("th"))
+     )
+    ),
+    dashboardBody(
+        tabItems(
+            # First tab content
+            tabItem(tabName = "dashboard",
+                    fluidRow(
+                        box(plotOutput("plot1", height = 250)),
+                        
+                        box(
+                            title = "Controls",
+                            sliderInput("slider", "Number of observations:", 1, 100, 50)
+                        )
+                    )
+            ),
+            
+            # Second tab content
+            tabItem(tabName = "widgets",
+                    h2("Widgets tab content")
+            )
         )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    set.seed(122)
+    histdata <- rnorm(500)
+    
+    output$plot1 <- renderPlot({
+        data <- histdata[seq_len(input$slider)]
+        hist(data)
     })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
